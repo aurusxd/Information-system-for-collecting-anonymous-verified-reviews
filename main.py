f752c37b81eb944734fd2477d96b4ee11c2dd9f1
aurusxd <warface.clan01@gmail.com>
2026-06-02 23:45:18 +0700

    Добавлен uv-пакетный менеджер в проект, добавлен ruff линтер

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from information-system-for-collecting-anonymous-verified-reviews!")
+
+
+if __name__ == "__main__":
+    main()
--- src/services/box_service.py
+++ src/services/box_service.py
@@ -1,6 +1,5 @@
 from src.models.box import Box
-from src.models.user import User
-from src.utils.security import generate_uuid, generate_token, generate_auth_token
+from src.utils.security import generate_uuid, generate_token
 from sqlalchemy.orm import Session
 
 def create_box(db: Session, user_id: int | None = None) -> Box:
